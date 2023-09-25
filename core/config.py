6d11e8fddd97249272fb056d3867318c2373c08f
Jammo89 <ostalzovmichail@gmail.com>
2023-09-25 23:14:15 +0500

    add database sqlite and config and base model, product model and db helper

---
+++ core/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).parent.parent
+
+class Setting(BaseSettings):
+    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    #db_echo: bool = False
+    db_echo: bool = True
+
+settings = Setting()
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 import uvicorn
+
+from core.models import Base, db_helper
 from items_views.items_views import router as items_router
 from users.views import router as users_router
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(items_router)
 app.include_router(users_router)
+
 
 
 
